af521612229ae4a83ee4e334aefc010e9a511142
James Y <35856061+jmyardley@users.noreply.github.com>
2021-01-14 22:42:45 -0500

    populate projects

--- src/components/List.js
+++ src/components/List.js
@@ -5,16 +5,19 @@ function List(props) {
   return (
     <div>
       {props.projects.map(item => (
-        <div className="container">
-          <div className="row">
-            <h3>{item.name}</h3>
-          </div>
-          <div className="row">
-            <div className="col-md-4">
-              <img src={item.img} alt={item.name} style={{"padding": "10px", "float": "center", "width": "100%"}} />
+        <div>
+          <hr style={{ "height": "5", "color": "#E1E1E1", "background-color": "#E1E1E1" }} />
+          <div className="container">
+            <div className="row">
+              <h3>{item.name}</h3>
             </div>
-            <div className="col-md-8">
-              <p>{item.desc}<br/><a href={item.app}>App</a>, <a href={item.repo}>Repo</a></p>
+            <div className="row">
+              <div className="col-md-4">
+                <img src={item.img} alt={item.name} style={{ "padding": "10px", "float": "center", "width": "100%" }} />
+              </div>
+              <div className="col-md-8">
+                <p>{item.desc}<br /><a href={item.app}>App</a>, <a href={item.repo}>Repo</a></p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,6 +10,38 @@ function Projects() {
       desc: "Using Javascript to create and display a randomly generated password based on user-selected criteria.",
       app: "https://jmyardley.github.io/password-generator/",
       repo: "https://github.com/jmyardley/password-generator"
+    },
+    {
+      id: 2,
+      name: "Burger App",
+      img: "https://user-images.githubusercontent.com/35856061/104677468-752fc180-56b7-11eb-82ce-e4079a70c3dd.png",
+      desc: "Demonstrating the ability to take in user input through a form and interact with an SQL database using Node.js, ORM, Express, and Handlebars. Deployed via Heroku using the JAWS_DB SQL extension.",
+      app: "https://damp-mesa-62613.herokuapp.com/",
+      repo: "https://github.com/jmyardley/burger"
+    },
+    {
+      id: 3,
+      name: "Getaway Planner (Collaboration)",
+      img: "https://user-images.githubusercontent.com/73077219/99841538-57713080-2b3c-11eb-9174-2830d451af9c.png",
+      desc: "Getaway Planner is a web application designed to help the user find the best places to eat and drink (micro-breweries) in whatever town you plan on travelling to within the United States, while giving the weather forecast as well.",
+      app: "https://wmdane.github.io/getaway_planner/",
+      repo: "https://github.com/wmdane/getaway_planner"
+    },
+    {
+      id: 4,
+      name: "Weather Dashboard",
+      img: "https://user-images.githubusercontent.com/35856061/104677760-fedf8f00-56b7-11eb-915b-07ca60524b18.png",
+      desc: "Interacting with the OpenWeatherMap API to retrieve and display weather information for user-selected cities.",
+      app: "https://jmyardley.github.io/weather-dashboard/",
+      repo: "https://github.com/jmyardley/weather-dashboard"
+    },
+    {
+      id: 5,
+      name: "Test Scheduling App (Collaboration)",
+      img: "https://user-images.githubusercontent.com/35856061/104678523-a27d6f00-56b9-11eb-9fb1-d8fc640b70cb.png",
+      desc: "Implementing Passport authentication, JawsDB, and the FullCalendar package to create a user portal to register for a medical test.",
+      app: "https://thawing-beach-04875.herokuapp.com/",
+      repo: "https://github.com/jmyardley/stellar-app"
     }
   ];
   return (
